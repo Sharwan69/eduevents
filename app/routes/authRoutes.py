b51fe1927c587fcef8fef1c6bdcc52569f32159d
Sharwan Sunar <sharwansunar91@gmail.com>
2026-06-05 20:54:39 +0545

    Delete app/routes/authRoutes.py

--- app/routes/authRoutes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-from controllers.authController import login
-
-
-auth_bp = Blueprint("auth", __name__, url_prefix="/api")
-
-
-auth_bp.add_url_rule("/login", view_func=login, methods=["POST"])
