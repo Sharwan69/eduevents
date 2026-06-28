66d4b76dfd490c43989ca21aa0460084522a5687
Sharwan Sunar <sharwansunar91@gmail.com>
2026-06-28 14:47:38 +0000

    Enhance authentication routes

--- app/routes/authRoutes.py
+++ app/routes/authRoutes.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
 
-from controllers.authController import login
+from controllers.authController import login, logout
 
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/api")
 
 
 auth_bp.add_url_rule("/login", view_func=login, methods=["POST"])
+auth_bp.add_url_rule("/logout", view_func=logout, methods=["POST"])
